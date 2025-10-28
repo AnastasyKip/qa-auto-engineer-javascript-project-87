2119632acc29486458145ba502f268604b0fda65
AnastasyKip <1746237@gmail.com>
2025-10-28 14:57:06 +0000

    implemented flat json diff

--- src/index.js
+++ src/index.js
@@ -1,18 +1,53 @@
 const { loadData } = require('./parsers');
 
-/**
- * Базовая функция сравнения:
- * - принимает пути к двум файлам и формат вывода (опционально)
- * - загружает и парсит данные
- * - возвращает строку (пока заглушка)
- */
-function genDiff(filepath1, filepath2, format = 'stylish') {
-  const data1 = loadData(filepath1);
-  const data2 = loadData(filepath2);
+const formatValue = (v) => {
+  if (typeof v === 'string') return v;
+  return String(v);
+};
 
-  // На следующем шаге здесь будет реальный дифф.
-  // Сейчас вернём читаемую заглушку, чтобы видеть, что парсинг отработал.
-  return `Parsed OK (${format}). Left keys: ${Object.keys(data1).length}, Right keys: ${Object.keys(data2).length}`;
+
+function buildFlatDiffString(obj1, obj2) {
+  const indent = '  ';
+
+  // Собираем уникальные ключи и сортируем по алфавиту (иммутабельно)
+  const keys = Array.from(new Set([...Object.keys(obj1), ...Object.keys(obj2)])).sort();
+
+  const lines = keys.flatMap((key) => {
+    const has1 = Object.prototype.hasOwnProperty.call(obj1, key);
+    const has2 = Object.prototype.hasOwnProperty.call(obj2, key);
+
+    if (has1 && has2) {
+      const val1 = obj1[key];
+      const val2 = obj2[key];
+
+      if (val1 === val2) {
+        // неизменённое поле
+        return `${indent}  ${key}: ${formatValue(val1)}`;
+      }
+      // изменённое поле: сначала из первого файла, затем из второго
+      return [
+        `${indent}- ${key}: ${formatValue(val1)}`,
+        `${indent}+ ${key}: ${formatValue(val2)}`,
+      ];
+    }
+
+    if (has1) {
+      // только в первом
+      return `${indent}- ${key}: ${formatValue(obj1[key])}`;
+    }
+
+    // только во втором
+    return `${indent}+ ${key}: ${formatValue(obj2[key])}`;
+  });
+
+  return `{\n${lines.join('\n')}\n}`;
 }
 
-module.exports = genDiff; // default export (CommonJS)
+
+function genDiff(filepath1, filepath2 /* , format = 'stylish' */) {
+  const data1 = loadData(filepath1);
+  const data2 = loadData(filepath2);
+  return buildFlatDiffString(data1, data2);
+}
+
+module.exports = genDiff;
