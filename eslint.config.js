949b2169261c92abf911ddf74aa86a1111d4518f
AnastasyKip <1746237@gmail.com>
2026-01-20 22:58:24 +0000

    fix eslint config, unify js modules, add prettier config

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+module.exports = [
+  {
+    ignores: ["node_modules/**", "coverage/**"],
+  },
+  {
+    files: ["**/*.js", "**/*.cjs"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "commonjs",
+    },
+    rules: {},
+  },
+];
